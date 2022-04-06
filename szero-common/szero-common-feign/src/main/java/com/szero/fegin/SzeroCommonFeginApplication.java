a7d66be098f7ac33652f6022707d1cac2a2e3fc9
张洋 <yang.zhang99@outlook.com>
2022-04-06 18:06:56 +0800

    [update] init

---
+++ szero-common/szero-common-feign/src/main/java/com/szero/fegin/SzeroCommonFeginApplication.java
@@ -0,0 +1,13 @@
+//package com.szero.fegin;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class SzeroCommonFeginApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(SzeroCommonFeginApplication.class, args);
+//    }
+//
+//}
