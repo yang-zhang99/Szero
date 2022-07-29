35951c8f9ff0ef19e2a6710e53bf8198a7c1ab9d
张洋 <yang.zhang99@outlook.com>
2022-07-29 11:19:24 +0800

    ttdo-gateway 使用

--- szero-modules/ttdo-oauth/src/test/java/com/ttdo/oauth/TtdoOauthApplicationTests.java
+++ szero-modules/ttdo-oauth/src/test/java/com/ttdo/oauth/TtdoOauthApplicationTests.java
@@ -1,13 +1,13 @@
-package com.ttdo.oauth;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class TtdoOauthApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.ttdo.oauth;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class TtdoOauthApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
