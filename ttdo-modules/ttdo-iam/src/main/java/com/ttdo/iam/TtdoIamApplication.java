c8696e74621b691014dd1c99ebf2a896e060441f
张洋 <yang.zhang99@outlook.com>
2022-08-10 20:00:55 +0800

    新增 iam 服务

--- ttdo-modules/ttdo-iam/src/main/java/com/ttdo/iam/TtdoIamApplication.java
+++ ttdo-modules/ttdo-iam/src/main/java/com/ttdo/iam/TtdoIamApplication.java
@@ -1,13 +1,13 @@
-package com.yang.jdbc;
+package com.ttdo.iam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SzeroDesignPatternsApplication {
+public class TtdoIamApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SzeroDesignPatternsApplication.class, args);
+        SpringApplication.run(TtdoIamApplication.class, args);
     }
 
 }
