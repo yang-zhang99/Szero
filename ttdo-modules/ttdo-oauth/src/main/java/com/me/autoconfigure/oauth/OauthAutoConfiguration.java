6ef867f711e32a2bac8b92dc29f719f1edec876e
张洋 <zhangyang@sxlyb.com>
2022-08-26 19:20:45 +0800

    TODO

--- ttdo-modules/ttdo-oauth/src/main/java/com/me/autoconfigure/oauth/OauthAutoConfiguration.java
+++ ttdo-modules/ttdo-oauth/src/main/java/com/me/autoconfigure/oauth/OauthAutoConfiguration.java
@@ -1,27 +1,23 @@
 package com.me.autoconfigure.oauth;
 
 
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
-@ComponentScan(value = {
-        "com.me.oauth.security",
-        "com.me.oauth.api",
-        "com.me.oauth.config",
-        "com.me.oauth.domain",
-        "com.me.oauth.infra",
-        "com.me.oauth.policy"
-})
-@MapperScan("com.me.oauth.infra.mapper")
-
-@EnableConfigurationProperties
+//@ComponentScan(value = {
+//        "com.me.oauth.security",
+//        "com.me.oauth.api",
+//        "com.me.oauth.config",
+//        "com.me.oauth.domain",
+//        "com.me.oauth.infra",
+//        "com.me.oauth.policy"
+//})
+//@MapperScan("com.me.oauth.infra.mapper")
+//@EnableConfigurationProperties
 @Configuration
-
-@EnableOAuth2Client
-
+//@EnableOAuth2Client
 public class OauthAutoConfiguration {
 
 
--- ttdo-modules/ttdo-oauth/src/main/java/com/me/oauth/TtdoOauthApplication.java
+++ ttdo-modules/ttdo-oauth/src/main/java/com/me/oauth/TtdoOauthApplication.java
@@ -1,17 +1,13 @@
 package com.me.oauth;
 
-import com.me.autoconfigure.oauth.EnableOauth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
-@EnableOauth
-
+//@EnableOauth
 @EnableDiscoveryClient
-
 @SpringBootApplication
-
 public class TtdoOauthApplication {
 
     public static void main(String[] args) {
