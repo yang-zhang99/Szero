f2f5de1a6d8ee97a336ae55343a96e0340ddda38
yang99 <yang.zhang99@outlook.com>
2022-09-24 14:11:05 +0800

    [Feature] init

--- ttdo-modules/ttdo-gateway/src/main/java/com/me/TtdoGateway.java
+++ ttdo-modules/ttdo-gateway/src/main/java/com/me/TtdoGateway.java
@@ -6,15 +6,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * 网关服务
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 
 @EnableGateway
-public class Gateway {
+public class TtdoGateway {
 
     public static void main(String[] args) {
 
-        new SpringApplicationBuilder(Gateway.class).web(WebApplicationType.REACTIVE).run(args);
+        new SpringApplicationBuilder(TtdoGateway.class).web(WebApplicationType.REACTIVE).run(args);
 
     }
 
